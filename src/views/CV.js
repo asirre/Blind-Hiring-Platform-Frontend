4453a068b5b25a141e9cf50fd4ba2a51b720f990
Anthony Kirkinis <akirkinis@gmail.com>
2023-03-19 22:30:01 +0100

    upload CV

--- src/views/CV.js
+++ src/views/CV.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { CButton, CCol, CCard, CCardText, CCardBody, CRow } from "@coreui/react";
 import BottomInfo from "../utils/BottomInfo";
 import Navbar from "../utils/Navbar";
@@ -7,27 +7,26 @@ import { uploadCV } from '../utils/upload_file_to_s3_example';
 
 const CV = () => {
 
-  // const [selectFileURL, setSelectFileURL] = useState();
+  const [selectFileURL, setSelectFileURL] = useState();
   const [selectFile, setSelectFile] = useState();
   
   const hiddenFileInput = React.useRef(null);
   
-  // const uploadFile = event => {
-  //   hiddenFileInput.current.click();
-  // };
+  const uploadFile = () => {
+    hiddenFileInput.current.click();
+  };
 
   function handleChange(event) {
     setSelectFile(event.target.files[0]);
+    setSelectFileURL(URL.createObjectURL(event.target.files[0]))
   }
 
-  const handleClick = (event) => { 
-    event.preventDefault();
+  const approveCV = async event => {
 
     if (selectFile != undefined) {
 
-      const formData = new FormData();
-      formData.append('selectFile', selectFile, selectFile.name);
-      console.log(selectFile.name);
+      var blob = new Blob([selectFile], {type : 'application/pdf'});
+      var fileOfBlob = new File([blob], selectFile.name);
 
 
       var config = {
@@ -35,40 +34,15 @@ const CV = () => {
         url: 'https://2etnadonz2.execute-api.eu-west-1.amazonaws.com/prod/cv-upload/'+ selectFile.name,
         headers: {
             'Authorization': 'eyJraWQiOiJSMFhMQW10MG9jOW5EeHR2YzRVRnViSmRkaDJvVGRMUXkrTStoOU1XYlRVPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjZmFjYTExMi1hOWEwLTQ2NTQtYjE5Ny1mN2RlN2Y4NDQzNzgiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0xX2x5N0V3M3VLbiIsImNvZ25pdG86dXNlcm5hbWUiOiJjZmFjYTExMi1hOWEwLTQ2NTQtYjE5Ny1mN2RlN2Y4NDQzNzgiLCJvcmlnaW5fanRpIjoiZDFhYTljYzEtZDRmMC00N2VkLWJjNzQtNTQ3NDNhMmVmYmQ5IiwiYXVkIjoiNDhvY21tZHNiZDZoYTFkOHBiZjI3amdkZWkiLCJldmVudF9pZCI6IjViNWNlNTM3LTA4YmMtNDZjNC1iNWNhLWJhN2VkMDVjZjIxYyIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjc4OTc1MzM3LCJleHAiOjE2Nzg5Nzg5MzcsImlhdCI6MTY3ODk3NTMzNywianRpIjoiODJhZTdhYjEtZWI4NC00NjVhLWExOTctMmE5ZDI0ODIyNjk5IiwiZW1haWwiOiJha2lya2luaXNAZ21haWwuY29tIn0.uBy5ksiWZbc3SYZ-4Ey4vMFVPR9Off3kIj0NiZDGqh8_9u7IIXfOYthsMtOM1D2mxb8uC18N3bfC2IGE0RSO9xjhN7_OF8sCzdqtwhM3Tus8mpqiOijFci5y0PvSykU917FvpujqRSbTmFbP9d8jksz-6ydM33layOlwQ-rR8oQVFbwZRlNuH_-l192LTgVdjXuRnWJy0giVRTG0TovmqpvCEy3kI1r0BIx42Ajkv2OdheQ5Ajze7scq7u9DScWnj3-VNMMuuW0R8k7WPv0RvHZJjLaECvJza3gEpbHoTwJkT1pcgJGXz81w6SxC7WMFenwrLZ0w3Y2Ynj1XaCiqNw',
-            // 'Content-Transfer-Encoding':'base64',
             'Content-Type': 'application/pdf'
         },
-        data : formData
+        data : fileOfBlob
       };
 
-      const response = uploadCV(config);
-      console.log(response);
+      const response = await uploadCV(config);
+      await console.log(response);
+      //redirect
     }
-    // this.handleFile(selectFile);
-  };
-
-  const approveCV = async event => {
-
-    // if (selectFile != undefined) {
-
-    //   const formData = new FormData();
-    //   formData.append('selectFile', selectFile, selectFile.name);
-    //   console.log(formData.name);
-
-
-    //   var config = {
-    //     method: 'PUT',
-    //     url: 'https://2etnadonz2.execute-api.eu-west-1.amazonaws.com/prod/cv-upload/'+ selectFile.name,
-    //     headers: {
-    //         'Authorization': 'eyJraWQiOiJSMFhMQW10MG9jOW5EeHR2YzRVRnViSmRkaDJvVGRMUXkrTStoOU1XYlRVPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjZmFjYTExMi1hOWEwLTQ2NTQtYjE5Ny1mN2RlN2Y4NDQzNzgiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0xX2x5N0V3M3VLbiIsImNvZ25pdG86dXNlcm5hbWUiOiJjZmFjYTExMi1hOWEwLTQ2NTQtYjE5Ny1mN2RlN2Y4NDQzNzgiLCJvcmlnaW5fanRpIjoiZDFhYTljYzEtZDRmMC00N2VkLWJjNzQtNTQ3NDNhMmVmYmQ5IiwiYXVkIjoiNDhvY21tZHNiZDZoYTFkOHBiZjI3amdkZWkiLCJldmVudF9pZCI6IjViNWNlNTM3LTA4YmMtNDZjNC1iNWNhLWJhN2VkMDVjZjIxYyIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjc4OTc1MzM3LCJleHAiOjE2Nzg5Nzg5MzcsImlhdCI6MTY3ODk3NTMzNywianRpIjoiODJhZTdhYjEtZWI4NC00NjVhLWExOTctMmE5ZDI0ODIyNjk5IiwiZW1haWwiOiJha2lya2luaXNAZ21haWwuY29tIn0.uBy5ksiWZbc3SYZ-4Ey4vMFVPR9Off3kIj0NiZDGqh8_9u7IIXfOYthsMtOM1D2mxb8uC18N3bfC2IGE0RSO9xjhN7_OF8sCzdqtwhM3Tus8mpqiOijFci5y0PvSykU917FvpujqRSbTmFbP9d8jksz-6ydM33layOlwQ-rR8oQVFbwZRlNuH_-l192LTgVdjXuRnWJy0giVRTG0TovmqpvCEy3kI1r0BIx42Ajkv2OdheQ5Ajze7scq7u9DScWnj3-VNMMuuW0R8k7WPv0RvHZJjLaECvJza3gEpbHoTwJkT1pcgJGXz81w6SxC7WMFenwrLZ0w3Y2Ynj1XaCiqNw',
-    //         'Content-Type': 'application/pdf'
-    //     },
-    //     data : formData
-    //   };
-
-    //   const response = await uploadCV(config);
-    //   console.log(response);
-    // }
     
   };
 
@@ -97,7 +71,7 @@ const CV = () => {
                     <CButton
                       style={{ color: "#2596be" }}
                       color="light"
-                      onClick={handleClick}
+                      onClick={uploadFile}
                     >
                       Upload
                     </CButton>
@@ -112,7 +86,7 @@ const CV = () => {
                     color: "white",
                     background: "transparent",
                     border: "none",
-                    fontSize: "2vw",
+                    fontSize: "4vw",
                   }}
                 >
                   <CCardBody>
@@ -120,10 +94,12 @@ const CV = () => {
                       <input
                         id='uploadedFile'
                         type="file"
+                        accept="application/pdf"
                         ref={hiddenFileInput}
                         onChange={handleChange}
-                        style={{display: 'true'}}
+                        style={{display: 'none'}}
                       />
+                      {selectFile?selectFile.name:""}
                     </CCardText>
                   </CCardBody>
                 </CCard>
@@ -148,13 +124,12 @@ const CV = () => {
               <CCardBody>
                 <CCardText >Overview:</CCardText>
 
-                {/* <embed style={{ width: "100%" }} src={selectFileURL} ref={filePreview}/> */}
+                <embed style={{ width: "100%" }} src={selectFileURL}/>
 
                 <CButton
                   style={{ color: "#2596be" }}
                   color="light"
-                  // onClick={approveCV}
-                  // href="/"
+                  onClick={approveCV}
                 >
                   Approve
                 </CButton>
--- src/views/UserPool.js
+++ src/views/UserPool.js
@@ -2,8 +2,8 @@ import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 
 const poolData = {
-  UserPoolId: REACT_APP_USER_POOL_ID,
-  ClientId: REACT_APP_POOL_CLIENT_ID
+  UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_POOL_CLIENT_ID
 }
 
 export default new CognitoUserPool(poolData);
