c2deeea6e1982b0223ee9a235e6b8f6590ff297c
Anthony Kirkinis <akirkinis@gmail.com>
2023-03-20 15:38:24 +0100

    fixes

--- src/views/CV.js
+++ src/views/CV.js
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { CButton, CCol, CCard, CCardText, CCardBody, CRow } from "@coreui/react";
 import BottomInfo from "../utils/BottomInfo";
 import Navbar from "../utils/Navbar";
 import { useNavigate } from "react-router-dom";
 import { uploadCV } from '../utils/upload_file_to_s3_example';
+import { AccountContext } from '../Account';
+import axios from 'axios';
 
 const CV = () => {
 
   const [selectFileURL, setSelectFileURL] = useState();
   const [selectFile, setSelectFile] = useState();
+  const [jwtToken, setjwtToken] = useState();
+  const [email, setEmail] = useState();
   
   const hiddenFileInput = React.useRef(null);
   const navigate = useNavigate();
+
+  const { getSession } = useContext(AccountContext);
   
   const uploadFile = () => {
     hiddenFileInput.current.click();
+    getSession();
   };
 
   function handleChange(event) {
     setSelectFile(event.target.files[0]);
-    setSelectFileURL(URL.createObjectURL(event.target.files[0]))
+    setSelectFileURL(URL.createObjectURL(event.target.files[0]));
   }
+
+
+  useEffect(()=> {
+    getSession()
+    .then(session => {
+      setjwtToken(session.idToken.jwtToken);
+      setEmail(session.idToken.payload.email); 
+    })
+  }, []);
 
   const approveCV = async event => {
 
@@ -34,19 +50,32 @@ const CV = () => {
         method: 'POST',
         url: 'https://2etnadonz2.execute-api.eu-west-1.amazonaws.com/prod/cv-upload/'+ selectFile.name,
         headers: {
-            'Authorization': 'eyJraWQiOiJSMFhMQW10MG9jOW5EeHR2YzRVRnViSmRkaDJvVGRMUXkrTStoOU1XYlRVPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjZmFjYTExMi1hOWEwLTQ2NTQtYjE5Ny1mN2RlN2Y4NDQzNzgiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0xX2x5N0V3M3VLbiIsImNvZ25pdG86dXNlcm5hbWUiOiJjZmFjYTExMi1hOWEwLTQ2NTQtYjE5Ny1mN2RlN2Y4NDQzNzgiLCJvcmlnaW5fanRpIjoiZDFhYTljYzEtZDRmMC00N2VkLWJjNzQtNTQ3NDNhMmVmYmQ5IiwiYXVkIjoiNDhvY21tZHNiZDZoYTFkOHBiZjI3amdkZWkiLCJldmVudF9pZCI6IjViNWNlNTM3LTA4YmMtNDZjNC1iNWNhLWJhN2VkMDVjZjIxYyIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjc4OTc1MzM3LCJleHAiOjE2Nzg5Nzg5MzcsImlhdCI6MTY3ODk3NTMzNywianRpIjoiODJhZTdhYjEtZWI4NC00NjVhLWExOTctMmE5ZDI0ODIyNjk5IiwiZW1haWwiOiJha2lya2luaXNAZ21haWwuY29tIn0.uBy5ksiWZbc3SYZ-4Ey4vMFVPR9Off3kIj0NiZDGqh8_9u7IIXfOYthsMtOM1D2mxb8uC18N3bfC2IGE0RSO9xjhN7_OF8sCzdqtwhM3Tus8mpqiOijFci5y0PvSykU917FvpujqRSbTmFbP9d8jksz-6ydM33layOlwQ-rR8oQVFbwZRlNuH_-l192LTgVdjXuRnWJy0giVRTG0TovmqpvCEy3kI1r0BIx42Ajkv2OdheQ5Ajze7scq7u9DScWnj3-VNMMuuW0R8k7WPv0RvHZJjLaECvJza3gEpbHoTwJkT1pcgJGXz81w6SxC7WMFenwrLZ0w3Y2Ynj1XaCiqNw',
+            'Authorization': jwtToken,
             'Content-Type': 'application/pdf'
         },
         data : fileOfBlob
       };
 
       await uploadCV(config)
-        .then( function (response) {
-          console.log(response);
-          navigate('/homepage');
-        }
+        .then( async function (response) {
+          var config = {
+            method: 'POST',
+            url: 'https://2etnadonz2.execute-api.eu-west-1.amazonaws.com/prod/user-metadata/'+ email,
+            headers: {
+                'Authorization': jwtToken
+            },
+            data: {
+              'cv' : selectFile.name
+            }
+          };
 
-      );      
+        await axios(config)
+        .then( function (response) {
+
+          navigate("/");
+        });
+        }
+      );     
     }
     
   };
