0ecea950a7e79d5df9000cb891c4ac9236e8686a
gosiaj <malgosia.janeczko@student.uva.nl>
2023-03-21 10:46:27 +0100

    fix

--- src/utils/Job.js
+++ src/utils/Job.js
@@ -9,7 +9,7 @@ import { cilArrowRight } from "@coreui/icons";
 const Job = ({ job, index }) => {
   return (
     <>
-      <div className="job-card w-full flex rounded overflow-hidden bg-slate-100 border border-gray-500">
+      <div className="job-card w-full flex rounded justify-between overflow-hidden bg-slate-100 border border-gray-500">
         <div className="job-details py-3 px-4 flex flex-col">
           <div className="job-header mb-2 flex gap-3">
             <div className="job-image">
@@ -21,7 +21,7 @@ const Job = ({ job, index }) => {
             </div>
           </div>
           <div className="job-description flex-1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+            <p>{job.description}</p>
           </div>
           
         </div>
