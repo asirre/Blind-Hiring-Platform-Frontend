787c84ebea15e56ad881af4830a5c4962dcf2e76
Radu <radu98@gmail.com>
2023-03-21 22:53:20 +0100

    fix typo

--- src/views/RouteGuard.js
+++ src/views/RouteGuard.js
@@ -16,13 +16,11 @@ const RouteGuard = () => {
             minHeight: "100vh" }}>
         <p className="text-center mt-56 text-9xl">404</p>
         <h1 className="text-center mt-24">Oops, you seem to be lost</h1>
-        <p className="text-center mt-12">This button will take you back to safety. Maybe you fogot to login?</p>
+        <p className="text-center mt-12">This button will take you back to safety. Maybe you forgot to login?</p>
         <div className="text-center">
           <CButton className="h-12 px-6 m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-lg" onClick={() => {startTransition(() => {navigate("/login")})}} >Back</CButton>
         </div>
       </CCol>
-
-      <BottomInfo />
     </>
   );
 };
